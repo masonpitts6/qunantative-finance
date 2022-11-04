826aabab9f35dff549a562caa386371de140f9ea
masonpitts6 <83966848+masonpitts6@users.noreply.github.com>
2022-11-04 08:38:48 -0400

    Implement mean variance plot and portfolio optimization

--- markowitz_model.py
+++ markowitz_model.py
@@ -1,26 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy.optimize as optimize
 import seaborn as sns
 import yfinance as yf
 
-stocks = ['AAPL', 'MSFT', 'AMZN', 'GOOG', 'META', 'TSLA', 'NFLX', 'NVDA', 'JPM', 'ADBE']
+stocks = ['SPY', 'AGG', 'EEM', 'VGK']
 
-start_date = '2015-01-01'
+start_date = '2010-01-01'
 end_date = '2022-10-31'
 
 NUM_TRADING_DAYS = 252
+NUM_PORTFOLIO = 100_000
 
 
-def download_stock_data(stocks, start_date, end_date):
+def download_stock_data(tickers, start_date, end_date):
     """
     Download data from Yahoo Finance
-    :param stocks: list of strings
+    :param tickers: list of strings
     :param start_date: string
     :param end_date: string
     :return: pandas DataFrame
     """
-    return yf.download(stocks, start=start_date, end=end_date)['Adj Close']
+    return yf.download(tickers, start=start_date, end=end_date)['Adj Close']
 
 
 def calculate_daily_returns(price_data):
@@ -128,19 +130,96 @@ def plot_line(data):
     data.plot(figsize=(10, 6))
     plt.show()
 
-def portfolio_mean_variance():
-    pass
 
+def portfolio_mean(returns, weights):
+    return np.sum(returns.mean() * weights) * NUM_TRADING_DAYS
+
+
+def portfolio_volatility(returns, weights):
+    return np.dot(weights.T, np.dot(returns.cov() * NUM_TRADING_DAYS, weights)) ** 0.5
+
+
+def generate_portfolios(returns):
+    portfolio_means = []
+    portfolio_vols = []
+    portfolio_weights = []
+
+    for _ in range(10000):
+        weight = np.random.random(len(returns.columns))
+        # Normalize weights
+        weight /= np.sum(weight)
+        portfolio_weights.append(weight)
+        portfolio_means.append(portfolio_mean(returns, weight))
+        portfolio_vols.append(portfolio_volatility(returns, weight))
+
+    return np.array(portfolio_means), np.array(portfolio_vols), np.array(portfolio_weights)
+
+
+def plot_portfolios(returns, volatilities):
+    plt.figure(figsize=(10, 6))
+    plt.scatter(volatilities, returns, c=returns / volatilities, marker='o')
+    plt.grid(True)
+    plt.xlabel('Expected Volatility')
+    plt.ylabel('Expected Return')
+    plt.colorbar(label='Sharpe Ratio')
+    plt.show()
+
+
+# Calculate negative sharpe ratio that is used to optimize the portfolio for mean variance.
+# Scipy can only minimize functions, not maximize so we need to minimize the negative sharpe ratio to maximize
+# the sharpe ratio.
+def negative_sharpe(weights, returns):
+    """
+    Calculate negative sharpe ratio
+    :param returns: pandas DataFrame
+    :param weights: numpy array
+    """
+    return -portfolio_mean(returns, weights) / portfolio_volatility(returns, weights)
+
+
+def optimize_portfolio_sharpe(returns, weights):
+    """
+    Optimize portfolio for sharpe ratio
+    :param returns: pandas DataFrame
+    :param weights: pandas DataFrame
+    :return:
+    """
+    # Sum of the weights must be 1
+    constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    # Weights must be between 0 and 1 since we are using long only positions
+    bounds = tuple((0, 1) for _ in range(len(returns.columns)))
+    return optimize.minimize(fun=negative_sharpe, x0=weights[0], args=returns, method='SLSQP', bounds=bounds,
+                             constraints=constraints)
+
+
+def print_optimal_portfolio(optimum, returns):
+    print(f"Optimal portfolio: {optimum['x'].round(3)}")
+
+
+def plot_optimal_portfolio(optimum, returns, portfolio_returns, portfolio_volatilities):
+    plt.figure(figsize=(10, 6))
+    plt.scatter(portfolio_volatilities, portfolio_returns, c=portfolio_returns / portfolio_volatilities, marker='o')
+    plt.grid(True)
+    plt.xlabel('Expected Volatility')
+    plt.ylabel('Expected Return')
+    plt.colorbar(label='Sharpe Ratio')
+    plt.plot(portfolio_volatility(returns, optimum['x']), portfolio_mean(returns, optimum['x']), 'r*', markersize=15.0)
+    plt.show()
 
 
 if __name__ == '__main__':
-    data = download_stock_data(stocks, start_date, end_date)
-    plot_line(data)
-    cumulative_returns = calculate_cumulative_returns(data)
+    df = download_stock_data(stocks, start_date, end_date)
+    plot_line(df)
+    cumulative_returns = calculate_cumulative_returns(df)
     plot_line(cumulative_returns)
-    log_returns = calculate_log_returns(data)
+    log_returns = calculate_log_returns(df)
     plot_line(log_returns)
-    returns = calculate_daily_returns(data)
-    stats = get_statistics(data)
+    s_returns = calculate_daily_returns(df)
+    stats = get_statistics(df)
     print(stats)
-    corr_matrix = calculate_correlation_matrix(data)
+    corr_matrix = calculate_correlation_matrix(df)
+    plot_correlation_matrix(corr_matrix)
+    p_means, p_vols, p_weights = generate_portfolios(log_returns)
+    plot_portfolios(p_means, p_vols)
+    p_optimum = optimize_portfolio_sharpe(log_returns, p_weights)
+    plot_optimal_portfolio(p_optimum, log_returns, p_means, p_vols)
